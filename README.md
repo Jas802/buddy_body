85094adfaf50fde3d3a22fa46e1a137bf75379b5
Jas802 <jas.spencer414@gmail.com>
2020-05-28 10:51:34 -0400

    Added scope method and adjusted show pages

--- README.md
+++ README.md
@@ -1,11 +1,6 @@
 To dos:
- <!-- Fix Omniauth
- Fix workout delete button
- Show Trainer's Workout Page
- Fix Flash Messaging
- Add Edit button for workouts
- -->
- Create Scope method
- Associations
- Variable scope
- params hashs
+Fix Add to my workouts
+Fix Omniauth
+
+Styling:
+Nav Bar
--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -2,8 +2,8 @@ class Trainer < ApplicationRecord
     has_many :workouts
     has_many :users, :through => :workouts
 
-    def self.most_difficult_workouts
-        where ('difficulty >= 4')
+    def most_difficult_workouts
+        self.workouts.where ('difficulty >= 4')
     end
 
 end
